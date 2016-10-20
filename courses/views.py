c398bd5d4cd8a1590d8ad43f2a90e1802136da0b
katya <root@admin>
2016-10-20 15:41:28 +0700

    Updates 20.10.16

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import RequestContext
+from django.template import loader
 from django.views.generic import ListView
 
 from courses.models import Course
@@ -5,3 +9,15 @@ from courses.models import Course
 
 class CourseListView(ListView):
     model = Course
+
+
+def detail(request, course_id):
+    output = Course.objects.filter(id=course_id)
+    # template = loader.get_template('courses/one_course.html')
+    # context = RequestContext(request, {
+    #     'one_course': output,
+    # })
+    # return HttpResponse(template.render(context))
+    # return HttpResponse(output)
+    context = {'one_course': output}
+    return render(request, 'courses/one_course.html', context)
