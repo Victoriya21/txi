439a3dde7b58a37cb5dd5ace5d7cdbc1776f108e
VSZ4 <zamiatina.v@gmail.com>
2016-10-27 11:31:08 +0700

    Trying to make adding of the file

--- labworks/urls.py
+++ labworks/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.conf import settings ##added
+from django.conf.urls.static import static ##added
 
 from . import views
 
 urlpatterns = [
     url(r'^create', views.create, name='create'),
     url(r'^(?P<lab_id>\d+)/$', views.detail, name='detail'),
+ ##   static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT), ##added
 ]
