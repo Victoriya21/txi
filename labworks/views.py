399a7a20cd454c2a3b65cb6bafd9921955210456
VSZ4 <zamiatina.v@gmail.com>
2016-11-01 21:48:24 +0700

    The Id of task passed for the labscreation form

--- labworks/views.py
+++ labworks/views.py
@@ -9,7 +9,7 @@ from .forms import LabForm
 from django.shortcuts import redirect
 
 
-def create(request):
+def create(request, task_id):
     if request.method == "POST":
         ##form = LabForm(request.POST)
         form = LabForm(request.POST, request.FILES)  ##added
@@ -21,7 +21,7 @@ def create(request):
           #  labwork.mark = 0
           #  labwork.condition = "не проверена"
             labwork.author = request.user
-            labwork.task = Task.objects.get(id=2)
+            labwork.task = Task.objects.get(id=task_id)
             labwork.save()
             return redirect('detail', lab_id=labwork.pk)
     else:
