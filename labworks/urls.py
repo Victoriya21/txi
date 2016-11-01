12c4f4afed84d0ed49898180816defed61b326f7
VSZ4 <zamiatina.v@gmail.com>
2016-11-01 21:48:24 +0700

    The Id of task passed for the labscreation form

--- labworks/urls.py
+++ labworks/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static ##added
 from . import views
 
 urlpatterns = [
-    url(r'^create', views.create, name='create'),
+  ##  url(r'^create', views.create, name='create'),
+    url(r'^create/(\d+)/$', views.create, name='create'),
     url(r'^(?P<lab_id>\d+)/$', views.detail, name='detail'),
  ##   static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT), ##added
 ]
