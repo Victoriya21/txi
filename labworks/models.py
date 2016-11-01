c75ca2e36cc0b2e1f972d3466a09d046b3ea3dbe
VSZ4 <zamiatina.v@gmail.com>
2016-11-01 18:11:25 +0700

    debugging the file loading

--- labworks/models.py
+++ labworks/models.py
@@ -7,9 +7,17 @@ from tasks.models import Task
 
 class Lab(models.Model):
     name = models.CharField(max_length=50)
-    mark = models.PositiveSmallIntegerField()
+    mark = models.PositiveSmallIntegerField(default=0)
     comment = models.TextField()
-    condition = models.CharField(max_length=50) #исправить?
+
+    CONDITION_CHECKED = 'checked'
+    CONDITION_NOT_CHECKED = 'not checked'
+    CONDITION_CHOICES = (
+        (CONDITION_CHECKED, 'Проверена'),
+        (CONDITION_NOT_CHECKED, 'Не проверена'),
+    )
+    condition = models.CharField(max_length=50, choices=CONDITION_CHOICES, default=CONDITION_NOT_CHECKED)  # исправить?
+
     ##file = models.CharField(max_length=250)
     file = models.FileField()
     task = models.ForeignKey(Task, related_name='tasks')
@@ -18,7 +26,7 @@ class Lab(models.Model):
     def __str__(self):
         return '{} on {}'.format(self.name, self.task)
 
-    def handle_uploaded_file(f):  ##added
-        with open('/srv/media/somefile.txt', 'wb+') as destination:
-            for chunk in f.chunks():
-                destination.write(chunk)
+  #  def handle_uploaded_file(f):  ##added
+  #      with open('/srv/media/somefile.txt', 'wb+') as destination:
+  #         for chunk in f.chunks():
+  #               destination.write(chunk)
