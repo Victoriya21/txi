0715487e6ee518a8c78b68aad327a144470842de
VSZ4 <zamiatina.v@gmail.com>
2016-11-05 14:23:15 +0700

    Made a task deleting

--- courses/views.py
+++ courses/views.py
@@ -6,6 +6,7 @@ from django.template import loader
 from django.views.generic import ListView
 
 from courses.models import Course
+from tasks.models import Task
 
 
 class CourseListView(ListView):
@@ -22,3 +23,9 @@ def detail(request, course_id):
 def delete(request, course_id):
     u = Course.objects.get(pk=course_id).delete()
     return HttpResponseRedirect("/courses")
+
+def deleteTask(request, course_id, task_id):
+    u = Task.objects.get(pk=task_id).delete()
+    output = get_object_or_404(Course, id=course_id)
+    context = {'one_course': output}
+    return render(request, 'courses/one_course.html', context)
