be9bc72a8b96ae6cf199de7f54dc63eae994d72e
gamboss <erdemgomboha@gmail.com>
2016-12-06 20:09:38 +0700

    Update urls.py
    для получения id курса не используя js

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^(?P<task_id>\d+)/$', views.detail, name='taskDetail'),
 
-    url(r'^create', views.create, name='create'),
+    url(r'^create/(\d+)/$', views.create, name='task_create'),
 
     url(r'^edit/(?P<task_id>[0-9]+)/$', views.edit, name='edit'),
 
