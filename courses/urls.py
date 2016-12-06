4c210cb23bf363ecf4cf742605a8a013b9cd4560
gamboss <erdemgomboha@gmail.com>
2016-12-06 19:52:30 +0700

    Update urls.py
    изменение URLов для метода reverse

--- courses/urls.py
+++ courses/urls.py
@@ -4,12 +4,12 @@ from . import views
 urlpatterns = [
     url(r'^$', views.CourseListView.as_view(), name='course_list'),
 
-    url(r'^(?P<course_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<course_id>\d+)/$', views.detail, name='course_detail'),
 
-    url(r'^create', views.create, name='create'),
+    url(r'^create', views.create, name='course_create'),
 
-    url(r'^edit/(?P<course_id>[0-9]+)/$', views.edit, name='edit'),
+    url(r'^edit/(?P<course_id>[0-9]+)/$', views.edit, name='course_edit'),
 
-    url(r'^delete/(\d+)/$', views.delete, name='delete'),
-    url(r'^(\d+)/delete/(\d+)/$', views.deleteTask, name='deleteTask'),
+    url(r'^delete/(\d+)/$', views.delete, name='course_delete'),
+    url(r'^(\d+)/delete/(\d+)/$', views.deleteTask, name='course_deleteTask'),
 ]
