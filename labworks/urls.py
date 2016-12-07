9638a8cd1e8ab0df9b97162c8b29ef36c46cbf8e
TImur Shabaev <antivirusnak@gmail.com>
2016-12-07 19:43:04 +0700

    update labworks url
    change labworks urls

--- labworks/urls.py
+++ labworks/urls.py
@@ -2,13 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^create/(\d+)/$', views.create, name='create'),
+    url(r'^create/(\d+)/$', views.create, name='lab_create'),
 
-    url(r'^edit/(\d+)/$', views.edit, name='edit'),
+    url(r'^edit/(\d+)/$', views.edit, name='lab_edit'),
 
-    url(r'^check/(\d+)/$', views.check, name='check'),
+    url(r'^check/(\d+)/$', views.check, name='lab_check'),
 
-    url(r'^download_file/(\d+)/$', views.download_file, name='download_file'),
-
-    url(r'^(?P<lab_id>\d+)/$', views.detail, name='detail'),
+    url(r'^download_file/(\d+)/$', views.download_file, name='download_file')
 ]
